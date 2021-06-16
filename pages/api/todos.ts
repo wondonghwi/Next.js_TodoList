3c27b79cdb0da4fad86724350ce48267787b322c
원동휘 <donghwi1289@gmail.com>
2021-06-16 23:57:26 +0900

    ADD : api 상태코드 추가

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.send('안녕 넥스트!');
+  if (req.method === 'GET') return res.send('Next.js!!');
+
+  res.statusCode = 405;
+  console.log(res.statusCode);
+  return res.end();
 };
