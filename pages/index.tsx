affbde4210a3ac3564ff697794a6208c1f33a99d
원동휘 <donghwi1289@gmail.com>
2021-06-16 23:51:12 +0900

    ADD : Next.js에서 express로 api 보내는 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,7 @@ import { NextPage } from 'next';
 import TodoList from '../components/TodoList';
 import { TodoType } from '../types/todo';
 
-const todos: TodoType[] = [
-  {
-    id: 1,
-    text: '코딩하기',
-    color: 'orange',
-    checked: false,
-  },
-  {
-    id: 2,
-    text: '공부하기',
-    color: 'navy',
-    checked: true,
-  },
-];
+const todos: TodoType[] = [];
 
 const app: NextPage = () => {
   return <TodoList todos={todos} />;
